9eb2b0880186c7f8fe1ccfcdb58e8e9085096048
Malik Luti <zwainie@edubuntu.(none)>
2015-06-26 03:11:37 -0700

    fourth attempt to deploy the code.

--- coffeedapp/wsgi.py
+++ coffeedapp/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeedapp.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeedapp.settings")
 
 application = Cling(get_wsgi_application())
 
